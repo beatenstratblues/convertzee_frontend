2b2d13f44c10663ba087e49406bb3060bf06e8b7
Jatin Singh <70967350+beatenstratblues@users.noreply.github.com>
2025-10-31 21:43:28 +0530

    On drag animation added

--- components/DropZone.js
+++ components/DropZone.js
@@ -24,11 +24,26 @@ const DropZone = () => {
     <section
       className={`${
         isDragActive ? "bg-blue-100" : "bg-gray-50"
-      } mt-8 p-8 border-2 border-dashed border-gray-400 rounded-lg text-center cursor-pointer w-3/6 mx-auto`}
+      } mt-12 p-8 border-2 border-dashed border-gray-400 rounded-lg text-center cursor-pointer w-6/6 mx-auto md:w-4/6 lg:w-3/6`}
     >
       <div {...getRootProps()}>
+        {acceptedFiles.length > 0 ? (
+          <aside className="mt-4">
+            <h4 className="font-semibold">Selected file:</h4>
+            <ul>
+              {acceptedFiles.map((file) => (
+                <li key={file.path}>
+                  {file.path} - {(file.size / 1024).toFixed(2)} KB
+                </li>
+              ))}
+            </ul>
+          </aside>
+        ) : isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag & drop files here, or click to select files</p>
+        )}
         <input {...getInputProps()} />
-        <p>Drag & drop files here, or click to select files</p>
       </div>
     </section>
   );

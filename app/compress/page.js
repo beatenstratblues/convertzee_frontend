8800308d2da62d8a1c6bae24a972dc5c4953d408
Jatin Singh <70967350+beatenstratblues@users.noreply.github.com>
2025-11-01 13:38:52 +0530

    Compression page done

--- app/compress/page.js
+++ app/compress/page.js
@@ -1,9 +1,21 @@
+import CompressButton from "@/components/CompressButton";
+import ConvertPageSelectorAndButton from "@/components/ConvertPageSelectorAndButton";
+import DropZone from "@/components/DropZone";
+
 const page = () => {
   return (
-    <div>
-        compress page
+    <div className="h-full">
+      <h1 className="text-4xl text-center pt-10 md:text-6xl md:pt-15">
+        Image Compressor
+      </h1>
+      <p className="text-center mt-3 md:text-xl">
+        Easily compress images online, safely!
+      </p>
+      <DropZone />
+      <p className="mt-5 text-center text-xs">Maximum file size should be 200 MB</p>
+      <CompressButton />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ components/CompressButton.js
@@ -0,0 +1,13 @@
+"use client";
+
+const CompressButton = () => {
+  return (
+    <div className="flex justify-center items-center mt-6">
+      <button className="ml-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-50">
+        Compress Image
+      </button>
+    </div>
+  );
+};
+
+export default CompressButton;
